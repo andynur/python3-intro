770116f982d7f6ec7c2d1175a8e0efe0228d3450
Andy Nur <andynur.id@gmail.com>
2025-09-01 21:20:52 +0700

    tambah contoh skrip dengan app.py

--- app.py
+++ app.py
@@ -2,3 +2,7 @@ print("BELAJAR DASAR GIT")
 # git add nama_file_yg_berubah
 # git commit -m "isi_pesan_perubahan"
 # git push -u origin main
+# --- cara tambah app.py file ---
+# git add app.py
+# git commit -m "add app file"
+# git push -u origin main
